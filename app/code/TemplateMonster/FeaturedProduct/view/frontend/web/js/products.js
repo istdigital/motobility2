59c92fa371d3d7d8de01559e7e4c0c89498069e8
rajalwaysfirst@gmail.com <rajalwaysfirst@gmail.com>
2020-03-05 19:30:42 +0530

    5mar

--- app/code/TemplateMonster/FeaturedProduct/view/frontend/web/js/products.js
+++ app/code/TemplateMonster/FeaturedProduct/view/frontend/web/js/products.js
@@ -11,25 +11,34 @@ define(['uiComponent','jquery','owlcarousel'], function(Component,$) {
                 data: {'type':this.name},
                 success: (function(htm) {
                     $('#'+this.name).html(htm);
+
+                    const icount = $('#'+this.name).find(".owl-carousel > .item").length;
+                    console.log(icount);
+
+
                     $('#'+this.name+" .owl-carousel").owlCarousel({
                         stagePadding: 5,
-                        loop:true,
+                        loop:false,
                         margin:40,
                         nav:true,
                         navText:["<i class='icon-arrow-prev'></i>","<i class='icon-arrow-next'></i>"],
                         responsive:{
                             0:{
                                 items:1,
-                                nav:false
+                                nav:false,
+                                loop:true,
                             },
                             414:{
-                                items:1
+                                items:1,
+                                loop:true,
                             },
                             600:{
-                                items:3
+                                items:3,
+                                loop: icount > 3 ? true : false,
                             },
                             1000:{
-                                items:4
+                                items:4,
+                                loop: icount > 4 ? true : false,
                             }
                         }
                     });
