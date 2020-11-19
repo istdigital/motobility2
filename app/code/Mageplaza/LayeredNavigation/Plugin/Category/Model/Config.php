4ead25617c4c405c2ef7398a6305be62df1eb871
Sarbjeet Singh <rajalwaysfirst@gmail.com>
2020-11-19 15:59:59 +0530

    -fix

---
+++ app/code/Mageplaza/LayeredNavigation/Plugin/Category/Model/Config.php
@@ -0,0 +1,23 @@
+<?php
+namespace Mageplaza\LayeredNavigation\Plugin\Category\Model;
+
+class Config {
+	public function afterGetAttributeUsedForSortByArray(
+		\Magento\Catalog\Model\Config $catalogConfig,
+		$options
+	) {
+		unset($options['position']);
+		unset($options['name']);
+		unset($options['price']);
+
+		$options['high_to_low'] = __('High To Low');
+		$options['low_to_high'] = __('Low To High');
+		$options['a_to_z'] = __('A - Z');
+		$options['z_to_a'] = __('Z - A');
+		//$options['position'] = __('Latest');
+
+		return $options;
+
+	}
+
+}
