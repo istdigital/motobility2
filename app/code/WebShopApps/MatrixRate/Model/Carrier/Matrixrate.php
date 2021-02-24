7594ba932eebe49bafa71ce0241ee83cfe9dd2d5
Sarbjeet Singh <rajalwaysfirst@gmail.com>
2021-02-24 15:11:53 +0530

    -fix

--- app/code/WebShopApps/MatrixRate/Model/Carrier/Matrixrate.php
+++ app/code/WebShopApps/MatrixRate/Model/Carrier/Matrixrate.php
@@ -37,245 +37,247 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Quote\Model\Quote\Address\RateRequest;
 
 class Matrixrate extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
-    \Magento\Shipping\Model\Carrier\CarrierInterface
-{
-    /**
-     * @var string
-     */
-    protected $_code = 'matrixrate';
+\Magento\Shipping\Model\Carrier\CarrierInterface {
+	/**
+	 * @var string
+	 */
+	protected $_code = 'matrixrate';
 
-    /**
-     * @var bool
-     */
-    protected $_isFixed = false;
+	/**
+	 * @var bool
+	 */
+	protected $_isFixed = false;
 
-    /**
-     * @var string
-     */
-    protected $defaultConditionName = 'package_weight';
+	/**
+	 * @var string
+	 */
+	protected $defaultConditionName = 'package_weight';
 
-    /**
-     * @var array
-     */
-    protected $conditionNames = [];
+	/**
+	 * @var array
+	 */
+	protected $conditionNames = [];
 
-    /**
-     * @var \Magento\Shipping\Model\Rate\ResultFactory
-     */
-    protected $rateResultFactory;
+	/**
+	 * @var \Magento\Shipping\Model\Rate\ResultFactory
+	 */
+	protected $rateResultFactory;
 
-    /**
-     * @var \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory
-     */
-    protected $resultMethodFactory;
+	/**
+	 * @var \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory
+	 */
+	protected $resultMethodFactory;
 
-    /**
-     * @var \WebShopApps\MatrixRate\Model\ResourceModel\Carrier\MatrixrateFactory
-     */
-    protected $matrixrateFactory;
+	/**
+	 * @var \WebShopApps\MatrixRate\Model\ResourceModel\Carrier\MatrixrateFactory
+	 */
+	protected $matrixrateFactory;
 
-    /**
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-     * @param \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory
-     * @param \Psr\Log\LoggerInterface $logger
-     * @param \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory
-     * @param \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $resultMethodFactory
-     * @param \WebShopApps\MatrixRate\Model\ResourceModel\Carrier\MatrixrateFactory $matrixrateFactory
-     * @param array $data
-     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
-     */
-    public function __construct(
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
-        \Psr\Log\LoggerInterface $logger,
-        \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory,
-        \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $resultMethodFactory,
-        \WebShopApps\MatrixRate\Model\ResourceModel\Carrier\MatrixrateFactory $matrixrateFactory,
-        array $data = []
-    ) {
-        $this->rateResultFactory = $rateResultFactory;
-        $this->resultMethodFactory = $resultMethodFactory;
-        $this->matrixrateFactory = $matrixrateFactory;
-        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
-        foreach ($this->getCode('condition_name') as $k => $v) {
-            $this->conditionNames[] = $k;
-        }
-    }
+	/**
+	 * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+	 * @param \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory
+	 * @param \Psr\Log\LoggerInterface $logger
+	 * @param \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory
+	 * @param \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $resultMethodFactory
+	 * @param \WebShopApps\MatrixRate\Model\ResourceModel\Carrier\MatrixrateFactory $matrixrateFactory
+	 * @param array $data
+	 * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+	 */
+	public function __construct(
+		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+		\Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
+		\Psr\Log\LoggerInterface $logger,
+		\Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory,
+		\Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $resultMethodFactory,
+		\WebShopApps\MatrixRate\Model\ResourceModel\Carrier\MatrixrateFactory $matrixrateFactory,
+		array $data = []
+	) {
+		$this->rateResultFactory = $rateResultFactory;
+		$this->resultMethodFactory = $resultMethodFactory;
+		$this->matrixrateFactory = $matrixrateFactory;
+		parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
+		foreach ($this->getCode('condition_name') as $k => $v) {
+			$this->conditionNames[] = $k;
+		}
+	}
 
-    /**
-     * @param RateRequest $request
-     * @return \Magento\Shipping\Model\Rate\Result
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     * @SuppressWarnings(PHPMD.NPathComplexity)
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-     */
-    public function collectRates(RateRequest $request)
-    {
-        if (!$this->getConfigFlag('active')) {
-            return false;
-        }
+	/**
+	 * @param RateRequest $request
+	 * @return \Magento\Shipping\Model\Rate\Result
+	 * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+	 * @SuppressWarnings(PHPMD.NPathComplexity)
+	 * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+	 */
+	public function collectRates(RateRequest $request) {
+		if (!$this->getConfigFlag('active')) {
+			return false;
+		}
 
-        // exclude Virtual products price from Package value if pre-configured
-        if (!$this->getConfigFlag('include_virtual_price') && $request->getAllItems()) {
-            foreach ($request->getAllItems() as $item) {
-                if ($item->getParentItem()) {
-                    continue;
-                }
-                if ($item->getHasChildren() && $item->isShipSeparately()) {
-                    foreach ($item->getChildren() as $child) {
-                        if ($child->getProduct()->isVirtual()) {
-                            $request->setPackageValue($request->getPackageValue() - $child->getBaseRowTotal());
-                        }
-                    }
-                } elseif ($item->getProduct()->isVirtual()) {
-                    $request->setPackageValue($request->getPackageValue() - $item->getBaseRowTotal());
-                }
-            }
-        }
+		$max_orderamount = $this->getConfigData('max_orderamount');
+		$minSubtotal = $request->getPackageValueWithDiscount();
 
-        // Free shipping by qty
-        $freeQty = 0;
-        if ($request->getAllItems()) {
-            $freePackageValue = 0;
-            foreach ($request->getAllItems() as $item) {
-                if ($item->getProduct()->isVirtual() || $item->getParentItem()) {
-                    continue;
-                }
+		if ($max_orderamount > 0 && $minSubtotal > $max_orderamount) {
+			return false;
+		}
 
-                if ($item->getHasChildren() && $item->isShipSeparately()) {
-                    foreach ($item->getChildren() as $child) {
-                        if ($child->getFreeShipping() && !$child->getProduct()->isVirtual()) {
-                            $freeShipping = is_numeric($child->getFreeShipping()) ? $child->getFreeShipping() : 0;
-                            $freeQty += $item->getQty() * ($child->getQty() - $freeShipping);
-                        }
-                    }
-                } elseif ($item->getFreeShipping()) {
-                    $freeShipping = is_numeric($item->getFreeShipping()) ? $item->getFreeShipping() : 0;
-                    $freeQty += $item->getQty() - $freeShipping;
-                    $freePackageValue += $item->getBaseRowTotal();
-                }
-            }
-            $oldValue = $request->getPackageValue();
-            $request->setPackageValue($oldValue - $freePackageValue);
-        }
+		// exclude Virtual products price from Package value if pre-configured
+		if (!$this->getConfigFlag('include_virtual_price') && $request->getAllItems()) {
+			foreach ($request->getAllItems() as $item) {
+				if ($item->getParentItem()) {
+					continue;
+				}
+				if ($item->getHasChildren() && $item->isShipSeparately()) {
+					foreach ($item->getChildren() as $child) {
+						if ($child->getProduct()->isVirtual()) {
+							$request->setPackageValue($request->getPackageValue() - $child->getBaseRowTotal());
+						}
+					}
+				} elseif ($item->getProduct()->isVirtual()) {
+					$request->setPackageValue($request->getPackageValue() - $item->getBaseRowTotal());
+				}
+			}
+		}
 
-        if (!$request->getConditionMRName()) {
-            $conditionName = $this->getConfigData('condition_name');
-            $request->setConditionMRName($conditionName ? $conditionName : $this->defaultConditionName);
-        }
+		// Free shipping by qty
+		$freeQty = 0;
+		if ($request->getAllItems()) {
+			$freePackageValue = 0;
+			foreach ($request->getAllItems() as $item) {
+				if ($item->getProduct()->isVirtual() || $item->getParentItem()) {
+					continue;
+				}
 
-        // Package weight and qty free shipping
-        $oldWeight = $request->getPackageWeight();
-        $oldQty = $request->getPackageQty();
+				if ($item->getHasChildren() && $item->isShipSeparately()) {
+					foreach ($item->getChildren() as $child) {
+						if ($child->getFreeShipping() && !$child->getProduct()->isVirtual()) {
+							$freeShipping = is_numeric($child->getFreeShipping()) ? $child->getFreeShipping() : 0;
+							$freeQty += $item->getQty() * ($child->getQty() - $freeShipping);
+						}
+					}
+				} elseif ($item->getFreeShipping()) {
+					$freeShipping = is_numeric($item->getFreeShipping()) ? $item->getFreeShipping() : 0;
+					$freeQty += $item->getQty() - $freeShipping;
+					$freePackageValue += $item->getBaseRowTotal();
+				}
+			}
+			$oldValue = $request->getPackageValue();
+			$request->setPackageValue($oldValue - $freePackageValue);
+		}
 
-        $request->setPackageWeight($request->getFreeMethodWeight());
-        $request->setPackageQty($oldQty - $freeQty);
+		if (!$request->getConditionMRName()) {
+			$conditionName = $this->getConfigData('condition_name');
+			$request->setConditionMRName($conditionName ? $conditionName : $this->defaultConditionName);
+		}
 
-        /** @var \Magento\Shipping\Model\Rate\Result $result */
-        $result = $this->rateResultFactory->create();
-        $zipRange = $this->getConfigData('zip_range');
-        $rateArray = $this->getRate($request, $zipRange);
+		// Package weight and qty free shipping
+		$oldWeight = $request->getPackageWeight();
+		$oldQty = $request->getPackageQty();
 
-        $request->setPackageWeight($oldWeight);
-        $request->setPackageQty($oldQty);
+		$request->setPackageWeight($request->getFreeMethodWeight());
+		$request->setPackageQty($oldQty - $freeQty);
 
-        $foundRates = false;
+		/** @var \Magento\Shipping\Model\Rate\Result $result */
+		$result = $this->rateResultFactory->create();
+		$zipRange = $this->getConfigData('zip_range');
+		$rateArray = $this->getRate($request, $zipRange);
 
-        foreach ($rateArray as $rate) {
-            if (!empty($rate) && $rate['price'] >= 0) {
-                /** @var \Magento\Quote\Model\Quote\Address\RateResult\Method $method */
-                $method = $this->resultMethodFactory->create();
+		$request->setPackageWeight($oldWeight);
+		$request->setPackageQty($oldQty);
 
-                $method->setCarrier('matrixrate');
-                $method->setCarrierTitle($this->getConfigData('title'));
+		$foundRates = false;
 
-                $method->setMethod('matrixrate_' . $rate['pk']);
-                $method->setMethodTitle(__($rate['shipping_method']));
+		foreach ($rateArray as $rate) {
+			if (!empty($rate) && $rate['price'] >= 0) {
+				/** @var \Magento\Quote\Model\Quote\Address\RateResult\Method $method */
+				$method = $this->resultMethodFactory->create();
 
-                if ($request->getFreeShipping() === true || $request->getPackageQty() == $freeQty) {
-                    $shippingPrice = 0;
-                } else {
-                    $shippingPrice = $this->getFinalPriceWithHandlingFee($rate['price']);
-                }
+				$method->setCarrier('matrixrate');
+				$method->setCarrierTitle($this->getConfigData('title'));
 
-                $method->setPrice($shippingPrice);
-                $method->setCost($rate['cost']);
+				$method->setMethod('matrixrate_' . $rate['pk']);
+				$method->setMethodTitle(__($rate['shipping_method']));
 
-                $result->append($method);
-                $foundRates = true; // have found some valid rates
-            }
-        }
+				if ($request->getFreeShipping() === true || $request->getPackageQty() == $freeQty) {
+					$shippingPrice = 0;
+				} else {
+					$shippingPrice = $this->getFinalPriceWithHandlingFee($rate['price']);
+				}
 
-        if (!$foundRates) {
-            /** @var \Magento\Quote\Model\Quote\Address\RateResult\Error $error */
-            $error = $this->_rateErrorFactory->create(
-                [
-                    'data' => [
-                        'carrier' => $this->_code,
-                        'carrier_title' => $this->getConfigData('title'),
-                        'error_message' => $this->getConfigData('specificerrmsg'),
-                    ],
-                ]
-            );
-            $result->append($error);
-        }
+				$method->setPrice($shippingPrice);
+				$method->setCost($rate['cost']);
 
-        return $result;
-    }
+				$result->append($method);
+				$foundRates = true; // have found some valid rates
+			}
+		}
 
-    /**
-     * @param \Magento\Quote\Model\Quote\Address\RateRequest $request
-     * @param bool $zipRange
-     * @return array|bool
-     */
-    public function getRate(\Magento\Quote\Model\Quote\Address\RateRequest $request, $zipRange)
-    {
-        return $this->matrixrateFactory->create()->getRate($request, $zipRange);
-    }
+		if (!$foundRates) {
+			/** @var \Magento\Quote\Model\Quote\Address\RateResult\Error $error */
+			$error = $this->_rateErrorFactory->create(
+				[
+					'data' => [
+						'carrier' => $this->_code,
+						'carrier_title' => $this->getConfigData('title'),
+						'error_message' => $this->getConfigData('specificerrmsg'),
+					],
+				]
+			);
+			$result->append($error);
+		}
 
-    /**
-     * @param string $type
-     * @param string $code
-     * @return array
-     * @throws \Magento\Framework\Exception\LocalizedException
-     */
-    public function getCode($type, $code = '')
-    {
-        $codes = [
-            'condition_name' => [
-                'package_weight' => __('Weight vs. Destination'),
-                'package_value' => __('Order Subtotal vs. Destination'),
-                'package_qty' => __('# of Items vs. Destination'),
-            ],
-            'condition_name_short' => [
-                'package_weight' => __('Weight'),
-                'package_value' => __('Order Subtotal'),
-                'package_qty' => __('# of Items'),
-            ],
-        ];
+		return $result;
+	}
 
-        if (!isset($codes[$type])) {
-            throw new LocalizedException(__('Please correct Matrix Rate code type: %1.', $type));
-        }
+	/**
+	 * @param \Magento\Quote\Model\Quote\Address\RateRequest $request
+	 * @param bool $zipRange
+	 * @return array|bool
+	 */
+	public function getRate(\Magento\Quote\Model\Quote\Address\RateRequest $request, $zipRange) {
+		return $this->matrixrateFactory->create()->getRate($request, $zipRange);
+	}
 
-        if ('' === $code) {
-            return $codes[$type];
-        }
+	/**
+	 * @param string $type
+	 * @param string $code
+	 * @return array
+	 * @throws \Magento\Framework\Exception\LocalizedException
+	 */
+	public function getCode($type, $code = '') {
+		$codes = [
+			'condition_name' => [
+				'package_weight' => __('Weight vs. Destination'),
+				'package_value' => __('Order Subtotal vs. Destination'),
+				'package_qty' => __('# of Items vs. Destination'),
+			],
+			'condition_name_short' => [
+				'package_weight' => __('Weight'),
+				'package_value' => __('Order Subtotal'),
+				'package_qty' => __('# of Items'),
+			],
+		];
 
-        if (!isset($codes[$type][$code])) {
-            throw new LocalizedException(__('Please correct Matrix Rate code for type %1: %2.', $type, $code));
-        }
+		if (!isset($codes[$type])) {
+			throw new LocalizedException(__('Please correct Matrix Rate code type: %1.', $type));
+		}
 
-        return $codes[$type][$code];
-    }
+		if ('' === $code) {
+			return $codes[$type];
+		}
 
-    /**
-     * Get allowed shipping methods
-     *
-     * @return array
-     */
-    public function getAllowedMethods()
-    {
-        return ['matrixrate' => $this->getConfigData('name')];
-    }
+		if (!isset($codes[$type][$code])) {
+			throw new LocalizedException(__('Please correct Matrix Rate code for type %1: %2.', $type, $code));
+		}
+
+		return $codes[$type][$code];
+	}
+
+	/**
+	 * Get allowed shipping methods
+	 *
+	 * @return array
+	 */
+	public function getAllowedMethods() {
+		return ['matrixrate' => $this->getConfigData('name')];
+	}
 }
